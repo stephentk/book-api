5c0971d8717bbfd3f27a274601e97b22d9366fd7
stephentk <84783886+stephentk@users.noreply.github.com>
2022-06-25 17:13:42 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import user_routes from "./handler/users";
 import product_routes from "./handler/product";
 import order_routes from "./handler/orders";
 
 const app = express();
 
-app.use(cors());
+
 app.use(bodyParser.json());
 user_routes(app);
 product_routes(app);
